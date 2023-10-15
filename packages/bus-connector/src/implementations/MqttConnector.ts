6eb8846f151fc02c47391b224f54972dba78739d
Dan Rumney <dancrumb@gmail.com>
2023-10-15 14:09:27 -0500

    feat(bus-connector): create MqttConnector
    resolves #17

--- packages/bus-connector/src/implementations/MqttConnector.ts
+++ packages/bus-connector/src/implementations/MqttConnector.ts
@@ -1,28 +1,65 @@
-import {connect, type MqttClient} from 'mqtt';
-import { BusConnector, BusMessage } from '../BusConnector.js';
+import { connectAsync, type MqttClient } from "mqtt";
+import { BusConnector, BusMessage, isBusMessage } from "../BusConnector.js";
 
 export class MqttConnector implements BusConnector {
-  private client: MqttClient;
-  private listeners: Map<string, Array<(message: BusMessage) => void>> = new Map();
+  private client: Promise<MqttClient>;
+  private listeners: Map<string, Array<(message: BusMessage) => void>> =
+    new Map();
 
-  constructor({url}:{url?: string}) {
-    const mqttUrl = url ?? process.env['MQTT_URL']??'';
-    this.client = connect(mqttUrl);
-  }
-    listenTo(topic: string, callback: (message: BusMessage) => void){
-        if(!this.listeners.has(topic)){
-            this.client.subscribe(topic);
-            this.listeners.set(topic, []);
+  constructor({ url }: { url?: string }) {
+    const mqttUrl = url ?? process.env["MQTT_URL"] ?? "";
+    this.client = connectAsync(mqttUrl);
+
+    this.client.then((c) => {
+      c.on("error", (e) => console.error(e));
+      c.on("message", (topic, message) => {
+        const messageAsString = message.toString();
+        try {
+          const parsedMessage = JSON.parse(messageAsString);
+          if (isBusMessage(parsedMessage)) {
+            this.listeners
+              .get(topic)
+              ?.forEach((callback) => callback(parsedMessage));
+          }
+        } catch (e) {
+          if (e instanceof SyntaxError) {
+            console.warn(
+              `Could not parse message '${messageAsString}' on topic ${topic}`
+            );
+          } else {
+            throw e;
+          }
         }
-        this.listeners.get(topic)?.push(callback);
-    };
+      });
+    });
+  }
 
-    stopListeningTo(topic: string) {
-        this.client.unsubscribe(topic);
-        this.listeners.delete(topic);
-    };
+  async awaitConnection() {
+    await this.client;
+  }
 
-    sendMessage (topic: string, message: BusMessage){
-        this.client.publish(topic, JSON.stringify(message));
+  async close() {
+    const client = await this.client;
+    await client.endAsync();
+  }
+
+  async listenTo(topic: string, callback: (message: BusMessage) => void) {
+    const client = await this.client;
+    if (!this.listeners.has(topic)) {
+      await client.subscribeAsync(topic);
+      this.listeners.set(topic, []);
     }
+    this.listeners.get(topic)?.push(callback);
+  }
+
+  async stopListeningTo(topic: string) {
+    const client = await this.client;
+    client.unsubscribe(topic);
+    this.listeners.delete(topic);
+  }
+
+  async sendMessage(topic: string, message: BusMessage) {
+    const client = await this.client;
+    client.publish(topic, JSON.stringify(message));
+  }
 }
