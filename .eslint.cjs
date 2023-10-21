f8c346f852661e6f8642a4495b3e44183bbaf409
Dan Rumney <dancrumb@gmail.com>
2023-10-21 14:34:47 -0500

    style(giotto): add ESLint and Prettier configs

--- .eslint.cjs
+++ .eslint.cjs
@@ -1,7 +1,20 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-  };
+  parser: '@typescript-eslint/parser',
+  "extends": [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  "parserOptions": {
+    project: ['./packages/*/tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  root: true,
+  "rules": {
+    "@typescript-eslint/strict-boolean-expressions": "error"
+  }
+};
